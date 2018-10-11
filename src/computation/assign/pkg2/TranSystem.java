16f7279a9251d1c586e23cc84e1ba04fcd204437
琉小璃 <@192.168.2.234>
2018-10-11 21:50:25 +1300

    关于数据结构Node的更新

---
+++ src/computation/assign/pkg2/TranSystem.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package computation.assign.pkg2;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author 琉小璃
+ */
+public class TranSystem {
+    public ArrayList<MapNode> init;
+    
+    public TranSystem() {
+        this.init = new ArrayList<>();
+    }
+    
+}
